4af1dafbbbe451770843db9d2717ea06d6cf986c
Kevin Viglucci <kviglucci@gmail.com>
2021-12-07 23:33:28 -0600

    fix: use identity serializer with composite metadata

--- src/main/javascript/socket.js
+++ src/main/javascript/socket.js
@@ -1,19 +1,27 @@
-import { BufferEncoders, JsonSerializer, RSocketClient } from "rsocket-core";
+import {
+    BufferEncoders,
+    JsonSerializer,
+    RSocketClient,
+    APPLICATION_JSON,
+    MESSAGE_RSOCKET_COMPOSITE_METADATA,
+    encodeRoute, MESSAGE_RSOCKET_ROUTING, encodeCompositeMetadata, IdentitySerializer
+} from "rsocket-core";
 import RSocketWebSocketClient from "rsocket-websocket-client";
 
-const client = new RSocketClient({
+const config = {
     serializers: {
         data: JsonSerializer,
-        metadata: JsonSerializer,
+        metadata: IdentitySerializer,
     },
     setup: {
         keepAlive: 999999,
         lifetime: 999999,
-        dataMimeType: "application/json",
-        metadataMimeType: "message/x.rsocket.routing.v0",
+        dataMimeType: APPLICATION_JSON.string,
+        metadataMimeType: MESSAGE_RSOCKET_COMPOSITE_METADATA.string,
         payload: {
-            // data: "hello",
-            metadata: String.fromCharCode("setup".length) + "setup"
+            metadata: encodeCompositeMetadata([
+                [MESSAGE_RSOCKET_ROUTING, encodeRoute("setup")],
+            ])
         }
     },
     transport: new RSocketWebSocketClient(
@@ -23,7 +31,9 @@ const client = new RSocketClient({
         },
         BufferEncoders,
     ),
-});
+};
+
+const client = new RSocketClient(config);
 
 client.connect().then(socket => {
     console.log('websocket connection established.');
